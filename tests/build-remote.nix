27a7a320747078c53811f81652aa8f135bf3b7e4
Robin <robin.lewis@anterior.com>
2025-09-20 00:58:02 -0400

    test: build immutable-js from source

--- tests/build-remote.nix
+++ tests/build-remote.nix
@@ -1,9 +1,6 @@
 # Packages can be built directly from a remote source.
 
 { package-lock2nix, nodejs_24 }:
-let
-  repo = builtins.fetchTree "github:anteriorai/brrr/2c2fe952144e01ab367221f074a02c8e6089762c";
-  # This package depends on node 24
-  package-lock2nix' = package-lock2nix.override { nodejs = nodejs_24; };
-in
-package-lock2nix'.mkNpmModule { src = "${repo}/typescript"; }
+package-lock2nix.mkNpmModule {
+  src = builtins.fetchTree "github:immutable-js/immutable-js/fce542f596e7c5908c93e7bfb0f6528d2fe3aec8";
+}
